7f6a3939db9f6202d30fe03821d391559c9d752f
ecamel1 <ecamel1@students.towson.edu>
2022-06-28 16:45:19 -0400

    4:45 lesson 3

--- Mern - NetNinja/backend/server.js
+++ Mern - NetNinja/backend/server.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 
 const express = require("express");
-const app = express(); // express app
+const workoutRoutes = require("./routes/workouts");
 
-// Routes
-app.get("/", (req, res) => {
-    res.json({mssg: "Welcome to the app"});
+ // express app
+const app = express();
+
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 });
 
+//routes
+app.use(workoutRoutes)
 //Listen for requests
 app.listen(process.env.PORT, () => {
-    console.log('listening on port 3000');
+    console.log("listening on port", process.env.PORT);
 });
