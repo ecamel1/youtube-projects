1c5feaf6176e2f4a36dde0286523bef048e994a2
ecamel1 <ecamel1@students.towson.edu>
2022-07-26 22:23:45 -0400

    #7 react review

--- NetNinja-React/blog/src/Home.js
+++ NetNinja-React/blog/src/Home.js
@@ -1,7 +1,22 @@
 function Home() {
+
+    const handleClick = (e) => {
+        console.log("Hello there", e);
+    };
+
+    // Both work
+    function handleClick2(name){
+        console.log("Hello " + name);
+    }
+
     return ( 
         <div className="Home">
             <h2> Homepage </h2>
+            {/* if you add handleClick() it will invoke the function on startup*/}
+            <button onClick={handleClick}>Click Me</button>
+
+            {/* Avoid it by making an anonymous function */}
+            <button onClick={() => handleClick2("Mario")}>Click Me, The Sequel</button>
         </div>
     );
 }
