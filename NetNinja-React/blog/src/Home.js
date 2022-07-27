efe21057bfaccff0df03b0afa0513716dbd416d4
ecamel1 <ecamel1@students.towson.edu>
2022-07-26 23:06:36 -0400

    #10 outputting lists

--- NetNinja-React/blog/src/Home.js
+++ NetNinja-React/blog/src/Home.js
@@ -1,31 +1,20 @@
 import { useState } from 'react';
 
 function Home() {
-    //let name = "Mario";
-    const [name, setName] = useState("Mario");
-    const [age, setAge] = useState(20);
-
-    const handleClick = () => {
-        setName("Luigi");
-        setAge(30);
-    };
-
-    // Both work
-    function handleClick2(name){
-        console.log("Hello " + name);
-    }
+    const [blogs, setBlogs] = useState([
+        {title: "New Blog Website", body: "Today I created...", author: "Me", id: 1},
+        {title: "Developer Tips and Tricks", body: "These are 5 tips and tricks all developers should know...", author: "You", id: 2},
+        {title: "Learning Material", body: "List of Free Materials ....", author: "Them", id: 3}
+    ]);
 
     return ( 
-        <div className="Home">
-            <h2> Homepage </h2>
-            <p> Hello, my name is { name } and I am { age } years old.</p>
-            
-
-            {/* if you add handleClick() it will invoke the function on startup*/}
-            <button onClick={handleClick}>Click Me</button>
-
-            {/* Avoid it by making an anonymous function */}
-            <button onClick={() => handleClick2(name)}>Click Me, The Sequel</button>
+        <div className="Home">   
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
