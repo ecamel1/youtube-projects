a0d5ff0d09a0a89540ff09163dd4f03d3d420134
ecamel1 <ecamel1@students.towson.edu>
2022-07-30 17:13:24 -0400

    16 needs work

--- NetNinja-React/blog/src/Home.js
+++ NetNinja-React/blog/src/Home.js
@@ -2,13 +2,7 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 function Home() {
-    const [blogs, setBlogs] = useState([
-        {title: "New Blog Website", body: "Today I created...", author: "Me", id: 1},
-        {title: "Developer Tips and Tricks", body: "These are 5 tips and tricks all developers should know...", author: "You", id: 2},
-        {title: "Learning Material", body: "List of Free Materials ....", author: "Them", id: 3}
-    ]);
-
-    const [name, setName] = useState("Marioin");
+    const [blogs, setBlogs] = useState([null]);
 
     //Uncaught TypeError: handleDelete is not a function
     const handleDelete = (id) => {
@@ -18,16 +12,19 @@ function Home() {
     }
 
     useEffect(() => {
-        console.log("useEffect Runs");
-        console.log(name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            });
+    }, []);
 
     return ( 
         <div className="Home">   
             {/* Prop Example Below */}
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <button onClick={(() => setName("Luigi"))}>Change Name</button>
-            <p>{ name }</p>
+            {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
         </div>
     );
 }
