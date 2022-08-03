291969bc447f046ffacece27cbaa9d1865401e04
ecamel1 <ecamel1@students.towson.edu>
2022-08-02 22:51:04 -0400

    #19 handling Errors

--- NetNinja-React/blog/src/Home.js
+++ NetNinja-React/blog/src/Home.js
@@ -4,22 +4,34 @@ import BlogList from './BlogList';
 function Home() {
     const [blogs, setBlogs] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsLoading(false);
-            });
+                .then(res => {
+                    if(!res.ok){
+                        throw Error("Unable to Fetch the Data. Please reload.");
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsLoading(false);
+                    setError(null);
+                })
+                .catch((err) => {
+                    setIsLoading(false);
+                    setError(err.message);
+                })
     }, []);
 
     return ( 
-        <div className="Home">   
+        <div className="Home">  
+            {/* Error message only display if error isnt null */}
+            { error && <div>{ error }</div>} 
             {/* If loading is true do ... */}
             { isLoading && <div>Loading . . .</div>}
+            
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
