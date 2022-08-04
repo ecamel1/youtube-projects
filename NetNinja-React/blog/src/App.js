bec5317809b54adc25b5848b9232bcea64d2cd26
ecamel1 <ecamel1@students.towson.edu>
2022-08-03 23:37:34 -0400

    #21-22 Routes basics

--- NetNinja-React/blog/src/App.js
+++ NetNinja-React/blog/src/App.js
@@ -1,15 +1,22 @@
 import Navbar from "./Navbar";
 import Home from './Home';
+import { BrowserRouter as Router , Route, Switch } from 'react-router-dom';
+import Create from './Create';
 
 //We can write as much javascript as we want before the return();
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/create"><Create /></Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
