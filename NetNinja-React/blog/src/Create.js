c05eba918c0c6d4ac69f20023fca1d123628507f
ecamel1 <ecamel1@students.towson.edu>
2022-08-07 08:31:48 -0400

    #25-26 fetch blog data by id and reuse useFetch

--- NetNinja-React/blog/src/Create.js
+++ NetNinja-React/blog/src/Create.js
@@ -2,6 +2,23 @@ const Create = () => {
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form action="POST">
+                <label>Blog Title:</label>
+                <input 
+                    type="text"
+                    
+                />
+                <label>Blog Body:</label>
+                <textarea 
+                    required>
+                </textarea>
+                <label>Blog Author:</label>
+                <select>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     )
 }
